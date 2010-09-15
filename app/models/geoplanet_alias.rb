4a948a20a0b3572125f233f7c4ab93272ec48744
Sebastian Korfmann <skorfmann@skorfmann.fritz.box>
2010-09-15 12:23:49 +0200

    named scopes

--- app/models/geoplanet_alias.rb
+++ app/models/geoplanet_alias.rb
@@ -2,4 +2,14 @@ class GeoplanetAlias < ActiveRecord::Base
 
   belongs_to :geoplanet_place, :foreign_key => 'woeid', :primary_key => 'woeid'
 
+  named_scope :language, lambda {|code| {:conditions => {:language_code => code.to_s}}}
+
+  named_scope :grouped_by_woeid, {
+            :select => "COUNT(*) as count, geoplanet_aliases.*",
+            :group => "geoplanet_aliases.woeid, geoplanet_aliases.language_code"
+    }
+
+  named_scope :limit, lambda {|max| {:limit => max}}
+
+
 end
--- app/models/geoplanet_place.rb
+++ app/models/geoplanet_place.rb
@@ -8,6 +8,15 @@ class GeoplanetPlace < ActiveRecord::Base
   has_many :adjacencies, :class_name => 'GeoplanetAdjacency', :foreign_key => 'woeid'
   has_many :adjacent_places, :through => :adjacencies
 
+  named_scope :language, lambda {|code| {:conditions => {:language => code.to_s}}}
+  named_scope :select, lambda {|*columns| {:select => columns.map {|column| "geoplanet_places.#{column.to_s}"}.join(", ") } }
+
+  %w(continent country state city).each do |place_type|
+    instance_eval do
+      named_scope place_type.pluralize.to_sym, :conditions => {:place_type => place_type.capitalize }
+    end
+  end
+
   def self.build_ancestry_from_parent_ids! parent_id = nil, ancestry = nil
     parent_id = parent_id || 0
     self.base_class.all(:conditions => {:parent_woeid => parent_id}).each do |node|
@@ -17,5 +26,7 @@ class GeoplanetPlace < ActiveRecord::Base
       build_ancestry_from_parent_ids! node.id, if ancestry.nil? then "#{node.id}" else "#{ancestry}/#{node.id}" end
     end
   end
+
+
 
 end

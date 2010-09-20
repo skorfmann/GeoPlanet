5c569cbfc1f26470c94b3ee8e9b7fcf386d9db5b
Sebastian Korfmann <skorfmann@skorfmann.local>
2010-09-20 14:24:46 +0200

    grouping by url slugs

--- app/models/geoplanet_slug.rb
+++ app/models/geoplanet_slug.rb
@@ -2,4 +2,11 @@ class GeoplanetSlug < ActiveRecord::Base
 
   belongs_to :geoplanet_place, :foreign_key => "woeid", :primary_key => "woeid"
 
+  named_scope :grouped_by_slug, {
+          :select => "COUNT(*) AS count, geoplanet_slugs.*",
+          :group => "geoplanet_slugs.url",
+          #:limit => 20000,
+          :having => "count > 1",
+          :order => "count DESC"
+          }
 end
